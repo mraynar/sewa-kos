add610d4980aafd53be504a465221249477b8393
Muhammad Raynar <muhammadraynar@Muhammads-MacBook-Pro.local>
2026-02-24 20:12:30 +0700

    Initial commit: Laravel Tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="bg-white p-8 rounded-lg shadow-xl border-t-4 border-blue-500">
+        <h1 class="text-3xl font-bold text-gray-800">
+            Sewa Kos Dashboard 🏠
+        </h1>
+        <p class="text-blue-500 mt-2">Jika teks ini berwarna biru dan kotak ini punya bayangan, Tailwind SUKSES!</p>
+        <button class="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+            Cek Kamar
+        </button>
+    </div>
+</body>
+</html>
